33a872a73f89a7398bf913ba986fa323f5549958
miniyus <miniyu97@gmail.com>
2023-02-11 09:58:21 +0900

    fix: api import module name

--- routes/api.go
+++ routes/api.go
@@ -5,12 +5,12 @@ import (
 	"github.com/miniyus/gofiber/auth"
 	configure "github.com/miniyus/gofiber/config"
 	"github.com/miniyus/gofiber/database"
-	groups2 "github.com/miniyus/gofiber/groups"
-	jobs2 "github.com/miniyus/gofiber/jobs"
+	"github.com/miniyus/gofiber/groups"
+	"github.com/miniyus/gofiber/jobs"
 	"github.com/miniyus/gofiber/pkg/jwt"
 	rsGen "github.com/miniyus/gofiber/pkg/rs256"
 	"github.com/miniyus/gofiber/pkg/worker"
-	users2 "github.com/miniyus/gofiber/users"
+	"github.com/miniyus/gofiber/users"
 	"github.com/miniyus/gofiber/utils"
 	"gorm.io/gorm"
 	"path"
@@ -58,27 +58,27 @@ func Api(apiRouter app.Router, a app.Application) {
 
 	// 해당 라인 이후로는 auth middleware가 공통으로 적용된다.
 	apiRouter.Middleware(auth.Middlewares(authMiddlewareParam)...)
-	// job 메타 데이터에 user_id 추가
-	apiRouter.Middleware(jobs2.AddJobMeta(jDispatcher, db))
+	// jobs 메타 데이터에 user_id 추가
+	apiRouter.Middleware(jobs.AddJobMeta(jDispatcher, db))
 
 	apiRouter.Route(
-		jobs2.Prefix,
-		jobs2.Register(
-			jobs2.New(
+		jobs.Prefix,
+		jobs.Register(
+			jobs.New(
 				utils.RedisClientMaker(cfg.RedisConfig),
 				jDispatcher,
 			),
 		),
-	).Name("api.jobs")
+	).Name("api.jobss")
 
 	apiRouter.Route(
-		groups2.Prefix,
-		groups2.Register(groups2.New(db)),
+		groups.Prefix,
+		groups.Register(groups.New(db)),
 	).Name("api.groups")
 
 	apiRouter.Route(
-		users2.Prefix,
-		users2.Register(users2.New(db)),
+		users.Prefix,
+		users.Register(users.New(db)),
 	).Name("api.users")
 
 }
