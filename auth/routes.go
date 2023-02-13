20e772f0baf2cf7cd2df8a8c5825567d5bcfb070
miniyus <miniyu97@gmail.com>
2023-02-13 15:18:05 +0900

    fix: auth api 변경된 미들웨어 적용

--- auth/routes.go
+++ auth/routes.go
@@ -12,15 +12,12 @@ func Register(handler Handler, parameter MiddlewaresParameter) app.SubRouter {
 		router.Post("/register", handler.SignUp).Name("api.auth.register")
 		router.Post("/token", handler.SignIn).Name("api.auth.token")
 
-		authMiddlewares := Middlewares(parameter)
+		authMiddleware := Middleware(parameter)
 
-		meHandlers := append(authMiddlewares, handler.Me)
-		router.Get("/me", meHandlers...).Name("api.auth.me")
+		router.Get("/me", authMiddleware, handler.Me).Name("api.auth.me")
 
-		resetPassHandlers := append(authMiddlewares, handler.ResetPassword)
-		router.Patch("/password", resetPassHandlers...).Name("api.auth.password")
+		router.Patch("/password", authMiddleware, handler.ResetPassword).Name("api.auth.password")
 
-		revokeHandlers := append(authMiddlewares, handler.RevokeToken)
-		router.Delete("/revoke", revokeHandlers...).Name("api.auth.revoke")
+		router.Delete("/revoke", authMiddleware, handler.RevokeToken).Name("api.auth.revoke")
 	}
 }
