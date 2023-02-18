b11bd581234184cbe1df484015a2fa4f6018608c
miniyus <miniyu97@gmail.com>
2023-02-18 19:50:59 +0900

    feat: gorm 모델의 hooks 메서드를 외부에서 주입하여 사용할 수 있는 기능 추가

--- users/repository.go
+++ users/repository.go
@@ -15,16 +15,9 @@ type RepositoryStruct struct {
 	gormrepo.GenericRepository[entity.User]
 }
 
-func NewRepository(db *gorm.DB, ent ...entity.User) Repository {
-	var u entity.User
-	if len(ent) == 0 {
-		u = entity.User{}
-	} else {
-		u = ent[0]
-	}
-
+func NewRepository(db *gorm.DB) Repository {
 	return &RepositoryStruct{
-		gormrepo.NewGenericRepository[entity.User](db, u),
+		gormrepo.NewGenericRepository[entity.User](db, entity.User{}),
 	}
 }
 
